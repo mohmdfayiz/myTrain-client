20a192b42c72253bd8ab7541d242e04d6053ef4e
Mohammed fayis <90344973+mohmd-fayis@users.noreply.github.com>
2023-04-01 10:40:25 +0530

    baseurl updated

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://mytrain-server.cyclic.app/";
 
 export const getPopular = () => axios.get("/api/getPopular");
 export const getStations = () => axios.get("/api/getStations");
